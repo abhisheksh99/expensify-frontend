7346d74ae7f23c2ab6fa3cc2ff1a082cf7b6d9b8
abhisheksh99 <abhishekleo1234@gmail.com>
2025-08-27 11:18:08 -0400

    Dashboard and landing page complete

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Header from "../components/Header.jsx";
+import HeroSection from "../components/HeroSection.jsx";
+import ProductShowcase from "../components/ProductShowcase.jsx";
 
 const LandingPage = () => {
-  return (
-    <div>LandingPage</div>
-  )
+    return (
+        <div className="bg-white font-sans text-gray-800">
+            <Header />
+            <main>
+                <HeroSection/>
+                <ProductShowcase />
+            </main>
+        </div>
+    )
 }
 
-export default LandingPage
+export default LandingPage;
