e7e4b19efaa95820a4703889c0eb0d930505c960
abhisheksh99 <abhishekleo1234@gmail.com>
2025-08-26 23:44:52 -0400

    Frontend Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+
+import LandingPage from "./pages/LandingPage";
+import Home from "./pages/Home";
+import Income from "./pages/Income";
+import Expense from "./pages/Expense";
+import Category from "./pages/Category";
+import Filter from "./pages/Filter";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Toaster />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/home" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/filter" element={<Filter />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+const Root = () => {
+    const isAuthenticated = !!localStorage.getItem("token");
+    return isAuthenticated ? (
+        <Navigate to="/dashboard" />
+    ) : (
+      <Navigate to="/home" />
+    );
+}
+export default App;
